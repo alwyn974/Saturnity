300f489ed014e30f76c1869e94b6f927e5cc21ce
hugobaret <hugo.baret@epitech.eu>
2023-01-16 11:09:54 +0400

    ⚗️ Test Google-test:     - Run unit test on function in static library (.a)

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int multiply(int a, int b)
+{
+    return a*b;
+}
+
+float multiply(float a, float b)
+{
+    return a*b;
+}
+
+double multiply(double a, double b)
+{
+    return a*b;
+}
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by bareth on 16/01/23.
+//
+
+#ifndef TEST_MAIN_H
+#define TEST_MAIN_H
+
+int multiply(int a, int b);
+
+float multiply(float a, float b);
+
+double multiply(double a, double b);
+
+#endif //TEST_MAIN_H
