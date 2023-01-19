048854fcbf7a2bfb3c06b8668a8bbcdc9494a510
alwyn974 <alwyn974@gmail.com>
2023-01-19 20:24:20 +0400

    ➕ Add working boost library fetching (little tricky, but it's work)

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -8,9 +8,8 @@ if (NOT ${LIBRARY_NAME}_FIND_QUIETLY)
 endif ()
 
 set(BOOST_SUBMODULES "tools/cmake;tools/boostdep;tools/boost_install" CACHE STRING "Boost submodules to fetch" FORCE)
-set(boost_libraries_required "" CACHE STRING "Required Boost libraries" FORCE)
 message(STATUS "Boost Components: ${${LIBRARY_NAME}_FIND_COMPONENTS}")
-foreach(submodule ${${LIBRARY_NAME}_FIND_COMPONENTS})
+foreach (submodule ${${LIBRARY_NAME}_FIND_COMPONENTS})
     string(SUBSTRING "${submodule}" 0 8 prefix)
     list(FIND BOOST_SUBMODULES "libs/${submodule}" index)
     if (index EQUAL -1)
@@ -20,7 +19,7 @@ foreach(submodule ${${LIBRARY_NAME}_FIND_COMPONENTS})
         message(STATUS "Adding submodule ${submodule}")
         list(APPEND BOOST_SUBMODULES "libs/${submodule}")
     endif ()
-endforeach()
+endforeach ()
 message(STATUS "Submodules: ${BOOST_SUBMODULES}")
 
 set(BOOST_INCLUDE_LIBRARIES ${${LIBRARY_NAME}_FIND_COMPONENTS}) # enabled libraries
@@ -38,27 +37,9 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(${LIBRARY_NAME})
 FetchContent_GetProperties(${LIBRARY_NAME})
 
-foreach(component ${${LIBRARY_NAME}_FIND_COMPONENTS})
-    message(STATUS "Adding component ${component}")
-    get_target_property(linked_targets Boost::${component} INTERFACE_LINK_LIBRARIES) # Get the linked libraries of Saturnity
+Include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/boost.utils.cmake)
 
-    if (NOT linked_targets)
-        message(STATUS "No linked targets for ${component}")
-        continue()
-    endif ()
-
-    # Creating a list of all the boost library required by the component
-    foreach(required_lib ${linked_targets})
-        string(REGEX REPLACE "Boost::" "" required_lib ${required_lib})
-        list(FIND boost_libraries_required "${required_lib}" index)
-        if (index EQUAL -1)
-            list(APPEND boost_libraries_required "${required_lib}")
-        endif ()
-    endforeach()
-
-    message(STATUS "Boost::${component} require: ${boost_libraries_required}")
-
-    #[[message(STATUS "Finding missing boost libraries")
-    set(Boost_FOUND FALSE CACHE BOOL "Boost found" FORCE)
-    find_package(Boost REQUIRED COMPONENTS ${boost_libraries_required})]]
-endforeach()
+foreach (component ${${LIBRARY_NAME}_FIND_COMPONENTS})
+    message(STATUS "Adding Boost component ${component}")
+    create_boost_dependency(Boost::${component})
+endforeach ()
