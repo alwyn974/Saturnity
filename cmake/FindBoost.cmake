dd197c3a9444ae9c03c6c17a19f96c1bf7eddcb3
alwyn974 <alwyn974@gmail.com>
2023-01-19 21:24:47 +0400

    ♻️ Refactoring...

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -12,21 +12,6 @@ if(POLICY CMP0135)
     cmake_policy(SET CMP0135 NEW)
 endif()
 
-#[[set(BOOST_SUBMODULES "tools/cmake;tools/boostdep;tools/boost_install" CACHE STRING "Boost submodules to fetch" FORCE)
-message(STATUS "Boost Components: ${${LIBRARY_NAME}_FIND_COMPONENTS}")
-foreach (submodule ${${LIBRARY_NAME}_FIND_COMPONENTS})
-    string(SUBSTRING "${submodule}" 0 8 prefix)
-    list(FIND BOOST_SUBMODULES "libs/${submodule}" index)
-    if (index EQUAL -1)
-        if (prefix STREQUAL "numeric_") # Fix for numeric_*
-            string(REPLACE "numeric_" "numeric/" submodule ${submodule})
-        endif ()
-        message(STATUS "Adding submodule ${submodule}")
-        list(APPEND BOOST_SUBMODULES "libs/${submodule}")
-    endif ()
-endforeach ()
-message(STATUS "Submodules: ${BOOST_SUBMODULES}")]]
-
 set(BOOST_INCLUDE_LIBRARIES ${${LIBRARY_NAME}_FIND_COMPONENTS}) # enabled libraries
 set(BOOST_ENABLE_CMAKE ON) # CMake support
 message(STATUS "Fetching Boost")
@@ -42,10 +27,3 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(${LIBRARY_NAME})
 FetchContent_GetProperties(${LIBRARY_NAME})
-
-#[[Include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/boost.utils.cmake)
-
-foreach (component ${${LIBRARY_NAME}_FIND_COMPONENTS})
-    message(STATUS "Adding Boost component ${component}")
-    create_boost_dependency(Boost::${component})
-endforeach ()]]
