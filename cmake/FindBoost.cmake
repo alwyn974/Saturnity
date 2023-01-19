98b070d0e015a35bd4f854b044f3f8b999bdf3b4
alwyn974 <alwyn974@gmail.com>
2023-01-19 21:10:59 +0400

    🔧 Change boost to archive (speed measurement)

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -7,7 +7,12 @@ if (NOT ${LIBRARY_NAME}_FIND_QUIETLY)
     set(FETCHCONTENT_QUIET OFF)
 endif ()
 
-set(BOOST_SUBMODULES "tools/cmake;tools/boostdep;tools/boost_install" CACHE STRING "Boost submodules to fetch" FORCE)
+# set the timestamps of extracted contents to the time of extraction
+if(POLICY CMP0135)
+    cmake_policy(SET CMP0135 NEW)
+endif()
+
+#[[set(BOOST_SUBMODULES "tools/cmake;tools/boostdep;tools/boost_install" CACHE STRING "Boost submodules to fetch" FORCE)
 message(STATUS "Boost Components: ${${LIBRARY_NAME}_FIND_COMPONENTS}")
 foreach (submodule ${${LIBRARY_NAME}_FIND_COMPONENTS})
     string(SUBSTRING "${submodule}" 0 8 prefix)
@@ -20,26 +25,27 @@ foreach (submodule ${${LIBRARY_NAME}_FIND_COMPONENTS})
         list(APPEND BOOST_SUBMODULES "libs/${submodule}")
     endif ()
 endforeach ()
-message(STATUS "Submodules: ${BOOST_SUBMODULES}")
+message(STATUS "Submodules: ${BOOST_SUBMODULES}")]]
 
 set(BOOST_INCLUDE_LIBRARIES ${${LIBRARY_NAME}_FIND_COMPONENTS}) # enabled libraries
 set(BOOST_ENABLE_CMAKE ON) # CMake support
 message(STATUS "Fetching Boost")
 FetchContent_Declare(
         ${LIBRARY_NAME}
-        GIT_REPOSITORY https://github.com/boostorg/boost.git
+        URL https://github.com/boostorg/boost/releases/download/boost-1.81.0/boost-1.81.0.tar.xz
+        #[[GIT_REPOSITORY https://github.com/boostorg/boost.git
         GIT_TAG boost-1.81.0
         GIT_SHALLOW 1 # Only fetch the head commit
         GIT_SUBMODULES ${BOOST_SUBMODULES}
         GIT_PROGRESS TRUE
-        CACHE
+        CACHE]]
 )
 FetchContent_MakeAvailable(${LIBRARY_NAME})
 FetchContent_GetProperties(${LIBRARY_NAME})
 
-Include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/boost.utils.cmake)
+#[[Include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/boost.utils.cmake)
 
 foreach (component ${${LIBRARY_NAME}_FIND_COMPONENTS})
     message(STATUS "Adding Boost component ${component}")
     create_boost_dependency(Boost::${component})
-endforeach ()
+endforeach ()]]
