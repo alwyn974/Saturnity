a35840f0ff49c5a31167a887f4b4d834179023a1
alwyn974 <alwyn974@gmail.com>
2023-01-19 13:03:16 +0400

    🚧 WIP 🔥 Remove clang-tidy llvm-header-guard check (it's a little broken) ➕ Add the good components management for SFML (TODO: boost) 🚧 WIP clang-tidy & clang-format cmake integration

--- cmake/FindSFML.cmake
+++ cmake/FindSFML.cmake
@@ -3,7 +3,9 @@ cmake_minimum_required(VERSION 3.17)
 set(LIBRARY_NAME SFML)
 
 Include(FetchContent)
-set(FETCHCONTENT_QUIET NO)
+if (NOT ${LIBRARY_NAME}_FIND_QUIETLY)
+    set(FETCHCONTENT_QUIET OFF)
+endif ()
 FetchContent_Declare(
         ${LIBRARY_NAME}
         GIT_REPOSITORY https://github.com/SFML/SFML.git
@@ -15,17 +17,18 @@ FetchContent_MakeAvailable(${LIBRARY_NAME})
 # Add the SFML components that you want to use in your project
 # For example, to use the graphics, window and system modules
 FetchContent_GetProperties(sfml)
-if (NOT sfml_POPULATED)
-    message(FATAL_ERROR "sfml not found")
-endif ()
-
-message(STATUS "${LIBRARY_NAME} is available now")
+message(STATUS "${LIBRARY_NAME} is available now ${${LIBRARY_NAME}_FIND_COMPONENTS}")
 
 set(SFML_BUILD_EXAMPLES OFF CACHE BOOL "")
 set(SFML_BUILD_DOC OFF CACHE BOOL "")
-set(SFML_BUILD_AUDIO ON CACHE BOOL "")
+set(SFML_BUILD_AUDIO OFF CACHE BOOL "")
 set(SFML_BUILD_NETWORK OFF CACHE BOOL "")
-set(SFML_BUILD_GRAPHICS ON CACHE BOOL "")
-set(SFML_BUILD_WINDOW ON CACHE BOOL "")
-set(SFML_BUILD_SYSTEM ON CACHE BOOL "")
+set(SFML_BUILD_GRAPHICS OFF CACHE BOOL "")
+set(SFML_BUILD_WINDOW OFF CACHE BOOL "")
+set(SFML_BUILD_SYSTEM OFF CACHE BOOL "")
 
+foreach(component ${${LIBRARY_NAME}_FIND_COMPONENTS})
+    string(TOUPPER ${component} component)
+    message(STATUS "Enabling ${component} for ${LIBRARY_NAME}")
+    set(SFML_BUILD_${component} ON CACHE BOOL "" FORCE)
+endforeach()
