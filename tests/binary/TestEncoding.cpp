266901b4dbf6efb81fae832fcca4fdb1fd0f0420
alwyn974 <alwyn974@gmail.com>
2023-01-20 14:08:48 +0400

    🚧♻️ WIP & Refactoring...

--- tests/binary/TestEncoding.cpp
+++ tests/binary/TestEncoding.cpp
@@ -7,6 +7,7 @@
 
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
+#include <boost/array.hpp>
 
 TEST(Encoding, TestEncodingInteger)
 {
@@ -18,4 +19,13 @@ TEST(Encoding, TestEncodingInteger)
 TEST(spdlog, TestSpdlog)
 {
     spdlog::info("Welcome to Saturnity!");
+}
+
+TEST(Boost, array)
+{
+    boost::array<int, 4> array = {1, 2, 3, 4};
+    ASSERT_EQ(array[0], 1);
+    ASSERT_EQ(array[1], 2);
+    ASSERT_EQ(array[2], 3);
+    ASSERT_EQ(array[3], 4);
 }
