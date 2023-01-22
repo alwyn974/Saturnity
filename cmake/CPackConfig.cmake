047ba76595e30fc18cc77ef9cdadf7992daa0b20
alwyn974 <alwyn974@gmail.com>
2023-01-22 18:32:08 +0400

    🚧 WIP CPackConfig.cmake

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -1,5 +1,26 @@
 include(InstallRequiredSystemLibraries) # include any runtime libraries that are needed by the project for the current platform
 
-#TODO: fully customize this file
+# Set some basic information about the project
+set(CPACK_PACKAGE_NAME "Saturnity")
+set(CPACK_PACKAGE_CONTACT "Alwyn974")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Saturnity is a library to handle networking")
+set(CPACK_PACKAGE_VENDOR "Alwyn974")
 
+# Set the version information
+set(CPACK_PACKAGE_VERSION "${PROJECT_VERSION}")
+set(CPACK_PACKAGE_VERSION_MAJOR "${PROJECT_VERSION_MAJOR}")
+set(CPACK_PACKAGE_VERSION_MINOR "${PROJECT_VERSION_MINOR}")
+set(CPACK_PACKAGE_VERSION_PATCH "${PROJECT_VERSION_PATCH}")
+set(CPACK_PACKAGE_VERSION_TWEAK "${PROJECT_VERSION_TWEAK}")
+
+# Set some resource files
+set(CPACK_RESOURCE_FILE_WELCOME "${CMAKE_CURRENT_SOURCE_DIR}/README.md") # TODO: change this later
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+
+# Set the package generators (7-Zip, Debian, NSIS, NuGet, Self Extract TGZ, TGZ, TXZ, WIX, ZIP)
+set(CPACK_SOURCE_GENERATOR "7Z;DEB;IFW;NSIS;NSIS64;NUGET;STGZ:TGZ;TXZ;WIX;ZIP")
+#set(CPACK_PACKAGE_INSTALL_DIRECTORY "${CPACK_PACKAGE_NAME}")
 include(CPack)
+
+get_cmake_property(res VARIABLES)
