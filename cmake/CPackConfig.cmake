1e4eb3feedf9216ee73791bfb728318bc0a4a8c4
alwyn974 <alwyn974@gmail.com>
2023-01-22 14:52:09 +0400

    ✅ Finish cmake install/export config (normally) ➕ Add logo for CPack (later)

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -1,3 +1,5 @@
+include(InstallRequiredSystemLibraries) # include any runtime libraries that are needed by the project for the current platform
+
+#TODO: fully customize this file
+
 include(CPack)
-include(GNUInstallDirs)
-include(InstallRequiredSystemLibraries)
