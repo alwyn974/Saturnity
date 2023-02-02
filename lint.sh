4badfd659a6d4c1b856cb8fb1234053fe9a2a070
alwyn974 <alwyn974@gmail.com>
2023-02-02 17:43:29 +0400

    ➕ Add clang-format-16 installation in lint.sh

--- lint.sh
+++ lint.sh
@@ -10,6 +10,20 @@ if [ -z "$(which fdfind)" ]; then
   fi
 fi
 
+if [ -z "$(which clang-format-16)" ]; then
+  echo "clang-format-16 not found, please install it"
+  read -r -p "The script can install it? [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    local tmp="$(mktemp -d)"
+    wget https://apt.llvm.org/llvm.sh -o "$tmp/llvm.sh"
+    chmod +x "$tmp/llvm.sh"
+    sudo "$tmp/llvm.sh" 16
+    sudo apt install clang-format-16 clang-tidy-16 -y
+  else
+    exit 1
+  fi
+fi
+
 echo "Running clang-format..."
 
 code="$(fdfind -E "cmake-*/" -i -e cpp -e hpp | xargs clang-format-16 -style=file --verbose --dry-run --Werror)"
@@ -20,7 +34,7 @@ else
   echo "Some files need to be formatted"
   read -r -p "Do you want to format it? [y/N] " response
   if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-    fdfind -E "cmake-*/" -i -e cpp -e hpp | xargs clang-format-16 -style=file -i
+    fdfind -E "cmake-*/" -i -e cpp -e hpp | xargs clang-format-16 -style=file --verbose -i
     exit 0
   else
     exit 1
