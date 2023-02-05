be20bcbb7e65d8900d5db5716d75aac6bc018db1
alwyn974 <alwyn974@gmail.com>
2023-02-05 20:28:16 +0400

    ♻️ Cleanup

--- src/Saturnity.cpp
+++ src/Saturnity.cpp
@@ -6,17 +6,3 @@
 */
 
 #include "saturnity/Saturnity.hpp"
-#include <iostream>
-
-class uwu {
-public:
-    void Outch() {
-        std::cout << "uwu" << std::endl;
-    }
-};
-
-enum class aestEnum {
-    a,
-    v,
-    C
-};
