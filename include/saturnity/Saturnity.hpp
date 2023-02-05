b9072407256d2e442aa38c611e46f2bf51080276
alwyn974 <alwyn974@gmail.com>
2023-02-05 20:28:16 +0400

    ♻️ Cleanup

--- include/saturnity/Saturnity.hpp
+++ include/saturnity/Saturnity.hpp
@@ -8,37 +8,11 @@
 #ifndef SATURNITY_SATURNITY_HPP
 #define SATURNITY_SATURNITY_HPP
 
-#include <spdlog/spdlog.h>
-#include <SFML/Graphics.hpp>
+/**
+ * @brief Header file for the Saturnity library. Include all the headers you need.
+ * @file Saturnity.hpp
+ */
 
-namespace sa {
-    class Test {
-    public:
-        Test() = default;
 
-        /**
-         * @brief Test function
-         */
-        inline void test() const
-        {
-            spdlog::info("Test");
-
-            sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-            sf::CircleShape shape(100.0);
-            shape.setFillColor(sf::Color::Green);
-
-            while (window.isOpen()) {
-                sf::Event event = {};
-                while (window.pollEvent(event)) {
-                    if (event.type == sf::Event::Closed)
-                        window.close();
-                }
-                window.clear();
-                window.draw(shape);
-                window.display();
-            }
-        }
-    };
-}
 
 #endif // SATURNITY_SATURNITY_HPP
