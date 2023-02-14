711161ae42baf8cbf3b6a66b6f26b2625b791068
alwyn974 <alwyn974@gmail.com>
2023-02-14 13:18:28 +0400

    🚧 WIP

--- main.cpp
+++ main.cpp
@@ -23,9 +23,77 @@ public:
     void fromBytes(sa::ByteBuffer &byteBuffer) override {};
 };
 
+class MessagePacket : public sa::AbstractPacket {
+public:
+    MessagePacket() : sa::AbstractPacket(sa::AbstractPacket::EnumPacketType::TCP) {};
+    explicit MessagePacket(const std::string &message) : sa::AbstractPacket(sa::AbstractPacket::EnumPacketType::TCP), _message(_message) {};
+    void toBytes(sa::ByteBuffer &byteBuffer) override {
+        byteBuffer.writeString(this->_message);
+    }
+    void fromBytes(sa::ByteBuffer &byteBuffer) override {
+        this->_message = byteBuffer.readString();
+    }
+
+    std::string getMessage() const {
+        return this->_message;
+    }
+private:
+    std::string _message;
+};
+
+class PacketReceivedEvent {
+public:
+    explicit PacketReceivedEvent(const std::shared_ptr<sa::AbstractPacket> &packet) : _packet(packet) {};
+
+    template<typename T>
+    std::shared_ptr<T> getPacket() const {
+        return this->_packet;
+    }
+
+private:
+    std::shared_ptr<sa::AbstractPacket> _packet;
+};
+
+class IPacketListener {
+public:
+    virtual ~IPacketListener() = default;
+};
+
+template<typename T, typename = std::enable_if_t<std::is_base_of<IPacketListener, T>::value, T>>
+class IPacket {
+public:
+    virtual ~IPacket() = default;
+
+    virtual void fromBytes(sa::ByteBuffer &byteBuffer) = 0;
+    virtual void toBytes(sa::ByteBuffer &byteBuffer) = 0;
+
+    virtual void handle(T &listener) = 0;
+};
+
+class EntityAddPacket;
+
+class GameListener : public IPacketListener {
+public:
+    void handleEntityAddPacket(EntityAddPacket &packet)
+    {
+        spdlog::info("EntityAddPacket");
+    }
+};
+
+class EntityAddPacket : public IPacket<GameListener> {
+public:
+    void fromBytes(sa::ByteBuffer &byteBuffer) override {}
+    void toBytes(sa::ByteBuffer &byteBuffer) override {}
+
+    void handle(GameListener &listener) override
+    {
+        listener.handleEntityAddPacket(*this);
+    }
+};
+
 int main(int ac, char **av)
 {
-    spdlog::info("Welcome to Saturnity!");
+    /*spdlog::info("Welcome to Saturnity!");
     spdlog::info("{}", sa::ByteBuffer::getVarShortSize(-4660));
 
     sa::ByteBuffer buffer(100);
@@ -47,10 +115,29 @@ int main(int ac, char **av)
     spdlog::info("{}", buffer.readString());
     spdlog::info("{}", buffer.readVarInt());
     spdlog::info("{}", buffer.size());
-
+*/
     sa::PacketRegistry packetRegistry;
     packetRegistry.registerPacket<Packet1>(0x1);
     packetRegistry.registerPacket<Packet2>(0x2);
+    packetRegistry.registerPacket<MessagePacket>(0x3);
+
+    sa::ByteBuffer buffer(100);
+    buffer.writeString("Hello World!");
+    // 0x3
+
+    std::unique_ptr<sa::AbstractPacket> packet = packetRegistry.createPacket(0x3, buffer);
+    try {
+        packetRegistry.createPacket<MessagePacket>(buffer);
+    } catch (std::exception &e) {
+        spdlog::error("Error caught: {}", e.what());
+    }
+
+    PacketReceivedEvent event(nullptr);
+    //event.getPacket<MessagePacket>()->getMessage();
+    GameListener listener;
+    std::shared_ptr<IPacket<GameListener>> entityAddPacket = std::make_shared<EntityAddPacket>();
+    // hmmm
+    entityAddPacket->handle(listener);
 
     return 0;
 }
